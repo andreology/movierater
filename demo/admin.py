d209d3a0643cfe2e9af5f216c13bb645802c8068
andreology <37252145+andreology@users.noreply.github.com>
2019-10-08 00:18:45 -0700

    Practiced making REST serializers templates and urls

--- demo/admin.py
+++ demo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+#customizing admin page
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['published']
+    search_fields = ['title', 'description']
--- movierater/settings.py
+++ movierater/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'demo',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'movierater.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
